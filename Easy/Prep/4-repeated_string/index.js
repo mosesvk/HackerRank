605b1708fefd4d76d06e4868734a4583b717964a
mosesvk <kaumatule93@gmail.com>
2022-09-14 22:32:46 -0600

    push

---
+++ Easy/Prep/4-repeated_string/index.js
@@ -0,0 +1,47 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Solutions ------------------------------------
+  // function repeatedString(s, n) {
+  //   let a = s.split('a').length - 1
+    
+  //   let rNum = n
+    
+  //   while (rNum % s.length != 0) {
+  //       rNum--
+  //   }
+    
+  //   let numOfA = rNum * a / s.length;
+    
+  //   let left = n - rNum;
+    
+  //   for (let i = 0; i < left; i++) {
+  //       if (s[i] === 'a')
+  //           numOfA++
+  //   }
+    
+  //   return numOfA
+  // }
