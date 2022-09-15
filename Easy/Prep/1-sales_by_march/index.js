f6a7257d1002a46fc9bed584a99b59a5dadbc213
mosesvk <kaumatule93@gmail.com>
2022-09-14 22:03:52 -0600

    push

---
+++ Easy/Prep/1-sales_by_march/index.js
@@ -0,0 +1,46 @@
+
+
+function sockMerchant(n, ar) {
+  // Write your code here
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// SOLUTIONS --------------------------------------------------
+
+  // function sockMerchant(n, ar) {
+  //   // Write your code here
+  //   let obj = {};
+  //   let pair = 0;
+  //   for (let num of ar) {
+  //       if (obj[num] === undefined) obj[num] = 1;
+  //       else obj[num] = obj[num] + 1;
+  //   }
+  //   for (let value of Object.values(obj)) {
+  //       pair += Math.floor(value / 2);
+  //   }
+
+  // }
