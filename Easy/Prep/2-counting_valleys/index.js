13c81f4363b2f14ae57e3e31a61cb69d28af6229
mosesvk <kaumatule93@gmail.com>
2022-10-01 14:20:10 -0600

    push

--- Easy/Prep/2-counting_valleys/index.js
+++ Easy/Prep/2-counting_valleys/index.js
@@ -3,9 +3,7 @@ function countingValleys(steps, path) {
   // Write your code here
   
   // Create Variables for the following: 
-      //'valleys' for the return integer of how many valleys traversed
-      //'traverse' for the integer that will chage depending on the 'path' array is going to return 'D' or 'U'
-      //Every time the 'traverse' integer goes from -1 to 0, then add 1 to the 'valleys' variable. 
+
   
   let valleys = 0
   let traverse = 0
